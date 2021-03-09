cb7ed41b39de9dd60e269cd2792184b3cccdec96
jane901201 <jane36742715@gmail.com>
2021-03-09 16:27:42 +0800

    OOP01

--- src/main/java/com/tom/HelloWorld.java
+++ src/main/java/com/tom/HelloWorld.java
@@ -1,7 +1,22 @@
 package com.tom;
 
 public class HelloWorld {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Student[] students = {
+                new Student("0001", "Jack", 30, 40),
+                new Student("0002", "June", 30, 40),
+                new Student("0003", "Tina", 30, 40),
+                new Student("0004", "Bobo", 30, 40)
+
+        };
+
+        for(int i =0; i < students.length; i++) {
+            students[i].print();
+        }
+
+
+//        System.out.println("Hello World");
     }
 }
--- src/main/java/com/tom/Student.java
+++ src/main/java/com/tom/Student.java
@@ -1,7 +1,21 @@
 package com.tom;
 
 public class Student {
-    public void say() {
-        System.out.println("Wait me a second");
-    }
+
+    String id;
+    String name;
+    int math;
+    int english;
+
+   public Student(String id, String name, int math, int english) {
+       this.id = id;
+       this.name = name;
+       this.english = english;
+       this.math = math;
+   }
+
+   public void print() {
+       System.out.println(id + "\t" + name + "\t" + english + "\t" + (english + math)/2);
+   }
+
 }
