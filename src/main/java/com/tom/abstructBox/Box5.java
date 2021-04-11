863e8be5af4e995201bb51fadb496975f1341903
jane901201 <75302011+jane901201@users.noreply.github.com>
2021-04-11 20:34:13 +0800

    abstructBox

--- src/main/java/com/tom/abstructBox/Box5.java
+++ src/main/java/com/tom/abstructBox/Box5.java
@@ -1,8 +1,14 @@
 package com.tom.abstructBox;
 
 public class Box5 extends Box{
+
+    public Box5(int length, int height, int width) {
+        this.length = length;
+        this.height = height;
+        this.width = width;
+    }
     @Override
     public String getName() {
-        return null;
+        return "Box5";
     }
 }
--- src/main/java/com/tom/abstructBox/BoxSeller.java
+++ src/main/java/com/tom/abstructBox/BoxSeller.java
@@ -1,4 +1,13 @@
 package com.tom.abstructBox;
 
 public class BoxSeller {
+
+    public static void main(String[] args) {
+        Box box3 = new Box3(3,5,6);
+        Box box5 = new Box5(3, 3, 3);
+
+        System.out.println(box3.getName());
+        System.out.println(box5.getName());
+    }
+
 }
